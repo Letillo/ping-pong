7135e6b0de25bb92def707777fc371dba6ce9405
Mauricio Trejos <mauriciotrejos89@gmail.com>
2020-09-13 04:05:14 -0600

    set score count and visualization on canvas box

--- client/js/gamelogic.js
+++ client/js/gamelogic.js
@@ -12,26 +12,37 @@ var y;
 var spdX;
 var spdY;
 
+// Score count variables
+var pointsOne = 0;
+
 // player object. Values are set with DB values.
 
 var player = {
 
-    id: 0,
-    name: "",
-    position: "",
-    x: 0,
-    y: 0,
-    width: 0,
-    height: 0
+  id: 0,
+  name: "",
+  position: "",
+  x: 0,
+  y: 0,
+  width: 0,
+  height: 0
 
 }
 
+// temp
+var score = {
+  x: 200,
+  y: 50,
+  points: 0
+};
 
 player.x = 10;
 player.y = 300;
 
 player.width = 10;
 player.height = 300;
+
+
 
 
 // hides the mouse cursor when it hovers the body section of the page.
@@ -42,60 +53,78 @@ updatePlayerPosition();
 
 
 function ballStartingPoint() {
-    // X and Y coords
-    x = window.innerWidth / 2;
-    y = Math.floor(Math.random() * window.innerHeight)
-    // var d give direction. if positive goes to the right.
-    var d = Math.round(Math.random());
-    (d == 0) ? d = 1 : d = -1;
-    spdX = 30 * d;
-    spdY = 2;
-    // ball draw
-    ctx.fillRect(x, y, 10, 10);
+  // X and Y coords
+  x = window.innerWidth / 2;
+  y = Math.floor(Math.random() * window.innerHeight)
+  // var d give direction. if positive goes to the right.
+  var d = Math.round(Math.random());
+  (d == 0) ? d = 1 : d = -1;
+  spdX = 30 * d;
+  spdY = 2;
+  // ball draw
+  ctx.fillRect(x, y, 10, 10);
+}
+
+function ballUpdate() {
+  // clear the previous ball point
+  ctx.clearRect(x, y, 10, 10);
+  // middle line field draw
+  ctx.strokeRect(window.innerWidth / 2, 0, 0, window.innerHeight);
+  ctx.fillStyle = "#ff0000";
+  x += spdX;
+  y += spdY;
+  // ball draw
+  ctx.fillRect(x, y, 10, 10);
+
+  if (x > canvas.width) { spdX = -20; }
+  if (x <= 0) {
+    spdX = 20;
+    scorePoint();
+    drawScore(score.points)
+    console.log(score.points);
   }
-  
-  function ballUpdate() {
-    // clear the previous ball point
-    ctx.clearRect(x, y, 10, 10);
-    // middle line field draw
-    ctx.strokeRect(window.innerWidth / 2, 0, 0, window.innerHeight);
-    ctx.fillStyle = "#ff0000";
-    x += spdX;
-    y += spdY;
-    // ball draw
-    ctx.fillRect(x, y, 10, 10);
-  
-    if (x > canvas.width) { spdX = -20; }
-    if (x < 0) { spdX = 20; }
-    if (y > canvas.height) { spdY = -20; }
-    if (y < 0) { spdY = 20; }
-  }
-  
-  
-  ballStartingPoint();
-  setInterval(ballUpdate, 1000 / 30);
-  
-  
+  if (y > canvas.height) { spdY = -20; }
+  if (y < 0) { spdY = 20; }
 
-function updatePlayerPosition (){
 
-    //fills rectangle with red color hexadecimals value
-    ctx.fillStyle = "#FF0000";    
+}
 
-    //creates a rectangle on x and y position
 
-    ctx.fillRect(player.x,player.y,player.width,player.height);    
+ballStartingPoint();
+setInterval(ballUpdate, 1000 / 30);
+
+
+
+function updatePlayerPosition() {
+
+  //fills rectangle with red color hexadecimals value
+  ctx.fillStyle = "#FF0000";
+
+  //creates a rectangle on x and y position
+
+  ctx.fillRect(player.x, player.y, player.width, player.height);
 
 }
 
 // add event listener to window. It listens on mouse movements. e parameter contains values of mouse position
 
-window.addEventListener('mousemove', e => {    
+window.addEventListener('mousemove', e => {
 
-    ctx.clearRect(player.x,player.y,player.width,player.height);
-    player.y = e.clientY-50;
-    
-    updatePlayerPosition();
-    
+  ctx.clearRect(player.x, player.y, player.width, player.height);
+  player.y = e.clientY - 50;
+
+  updatePlayerPosition();
+
 });
 
+function scorePoint() {
+  score.points++;
+  ballUpdate();
+}
+
+function drawScore() {
+  ctx.clearRect(score.x, (score.y - 25), 150, 25);
+  // ctx.fillRect(score.x, (score.y - 25), 150, 25);
+  ctx.font = "25px Arial";
+  ctx.fillText('Score: ' + score.points, score.x, score.y);
+}
